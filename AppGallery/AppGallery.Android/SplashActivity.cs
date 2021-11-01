99146b3186bc2b87b38259f54a5077f3fda4d1d0
Luís Felipe Rodrigues Valoura <luis@galltecnologia.com.br>
2021-11-01 08:46:43 -0300

    Adição e configuração dos Ícones e Logo para Adroid e UWP.

---
+++ AppGallery/AppGallery.Android/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppGallery.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // Create your application here
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+
+            // Desativar animação de transição
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
